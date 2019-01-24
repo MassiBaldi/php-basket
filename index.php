2baa13f854d31f52979b9aa7cf00eed3b7b6e90e
MassiBaldi <massibaldi85@gmail.com>
2019-01-24 17:57:50 +0100

    risolto

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
     <title>php-basket</title>
   </head>
   <body>
-    
+    <?php foreach ($giocatori as $giocatore) { ?>
+      <div class="card">
+        <ul>
+          <li>Codice giocatore: <?php echo $giocatore['giocatoreUnivoco'] ?></li>
+          <li>Punti Fatti: <?php echo $giocatore['numeroPunti'] ?></li>
+          <li>Rimbalzi Fatti: <?php echo $giocatore['numeroRimbalzi'] ?></li>
+          <li>Falli: <?php echo $giocatore['falli'] ?></li>
+          <li>Percentuale da 2 punti: <?php echo $giocatore['percentuale2punti'] ?></li>
+          <li>Percentuale da 3 punti: <?php echo $giocatore['percentuale3punti'] ?></li>
+        </ul>
+      </div>
+    <?php } ?>
   </body>
 </html>
